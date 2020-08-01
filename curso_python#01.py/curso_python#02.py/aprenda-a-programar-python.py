3b12e0cb04f875c1c1fc4c7e975f7535a4f35f3e
robinson-85 <robinpaulista@yahoo.com.br>
2020-08-01 11:01:31 -0300

    Curso python 01

---
+++ curso_python#01.py/curso_python#02.py/aprenda-a-programar-python.py
@@ -0,0 +1,175 @@
+De onde vem o Python?
+
+Python foi criado no final dos anos oitentas (1989) 
+por Guido van Rossum no Centro de Matemática e 
+Tecnológia da Informação (CWI, Centrum Wiskunde e 
+Informatica), na Holanda, como sucessor da linguagem 
+de programação ABC, capaz de lidar com exceções e 
+interagir com o sistema operacional Amoeba.
+
+O nome da língua vem do gosto de seu criador pelos
+humoristas britânicos Monty Python. Van Rossum é o
+principal autor de Python, e seu papel central 
+contínuo na decisão da direção de Python é reconhecido, 
+referindo-se a ele como Ditador de Vida Benevolente 
+(em inglês: Benevolent Dictator for Life, BDFL).
+
+Python é uma linguagem de programação interpretada 
+cuja filosofia enfatiza uma sintaxe favorecendo um 
+código mais legível, além de ser “free”.
+
+Cenário Atual do Python
+Atualmente, é gerenciada pela Python Software 
+Foundation. Possui uma licença de código aberto, 
+chamada Python Software Foundation License, que é 
+compatível com a GNU General Public License a partir
+ da versão 2.1.1 e incompatível em certas versões 
+ anteriores.
+
+É uma linguagem de programação multi-paradigma, pois 
+suporta orientação de objeto, programação imperativa e, 
+em menor escala, programação funcional.
+
+É uma linguagem interpretada, têm seus códigos 
+fontes transformados em uma linguagem intermediária (específica de cada linguagem), que será interpretada
+pela máquina virtual da linguagem quando o programa 
+for executado.
+
+Usa tipagem dinâmica e forte, isso, significa que o 
+próprio interpretador do Python infere o tipo dos dados 
+que uma variável recebe, sem a necessidade que o usuário 
+da linguagem diga de que tipo determinada variável é.
+
+Hoje existem inúmeras linguagens no mercado que são 
+fortemente tipadas, referenciando especificamente o 
+Python para explicar a questão: Tipagem forte significa 
+que o interpretador do Python avalia as expressões 
+(evaluate) e não faz coerções automáticas entre tipos 
+não compatíveis (conversões de valores), ou seja: 
+Quando recebemos uma exception chamada TypeError ou seja,
+ao fazer operações com tipos incompatíveis, o Python não converte automaticamente esses tipos pra você, ele vai dar erro! Isso é bom, pois assim você terá a certeza que o seu resultado é mais consistente.
+
+E é multiplataforma, funcionando igualmente bem em 
+plataformas Windows, GNULinux, UNIX e Mac/Apple, 
+assim como outras, indo desde supercomputadores 
+até telefones celulares. Pode ser usado para 
+desenvolver pequenas aplicações e protótipos 
+rápidos, mas escala bem para permitir o 
+desenvolvimento de programas robustos. 
+Vem com um conjunto de ferramentas para 
+Interface Gráfica do Usuário (GUI) poderoso 
+e fácil de usar, bibliotecas para programação 
+web, e muito mais.
+
+E, o melhor de tudo é grátis. Python foi feita 
+tendo com base na linguagem ABC, parte da sintaxe 
+derivada do C, compreensão de listas, funções 
+anônimas e função map derivadas do Linguagem Haskell.
+
+Os iteradores são baseados na Icon, tratamentos 
+de exceção e módulos da Modula-3, expressões 
+regulares de Perl. Em 1991, Guido publicou o código 
+(nomeado versão 0.9.0) no grupo de discussão 
+alt.sources(newsgroup).
+
+Nessa versão, adicionadas classes com herança, 
+tratamento de exceções, funções e os tipos de 
+dados nativos: list, dict, str, e assim por diante. 
+Como também um sistema de módulos emprestado do Modula-3.
+O modelo de exceções também lembrava muito o do Modula-3,
+com a adição da opção else. Em 1994, é formado o 
+principal grupo de discussão do Python,
+comp.lang.python.
+
+Os primeiros passos
+A versão 1.0 foi lançada em janeiro de 1994. 
+Novas funcionalidades foram incluidas para 
+programação funcional como lambda, map, filter 
+e reduce.
+
+A última versão enquanto Guido estava na CWI foi
+ o Python 1.2. Em 1995, trabalhando no CNRI em 
+ Reston, Estados Unidos, de onde lançou diversas 
+ atualizações. Na versão 1.4 a linguagem adicionou 
+ parâmetros nomeados e suporte nativo a números 
+ complexos, assim como uma forma de encapsulamento.
+
+Lançou a iniciativa Computer Programming for Everybody 
+(CP4E; literalmente, “Programação de Computadores para
+ Todos”), que visava tornar a programação mais acessível, 
+ um projeto financiado pela DARPA.
+
+Atualmente o CP4E encontra-se inativo. Em 2000, o time de desenvolvimento da linguagem se mudou para a BeOpen a fim
+ de formar o time PythonLabs.
+
+A CNRI pediu que a versão 1.6 fosse lançada, para marcar 
+o fim de desenvolvimento da linguagem naquele local.
+
+O único lançamento na BeOpen foi o Python 2.0, e após o lançamento o grupo de desenvolvedores da PythonLabs 
+agrupou-se na Digital Creations, implementou list comprehension, uma relevante funcionalidade de 
+linguagens funcionais como SETL e Haskell.
+
+A sintaxe da linguagem para essa construção é bastante 
+similar a de Haskell, exceto pela preferência do Haskell 
+por caracteres de pontuação e da preferência do python 
+por palavras reservadas alfabéticas. Também introduziu 
+um sistema coletor de lixo(Garbage Coletor) capaz de identificar e tratar ciclos de referências. Python 2.1 
+era parecido com as versões 1.6.1 e 2.0.
+
+Licenciamento
+Sua licença foi renomeada para Python Software 
+Foundation License. Todo código, documentação e 
+especificação desde o lançamento da versão alfa da
+ 2.1 é propriedade da Python Software Foundation (PSF), 
+ uma organização sem fins lucrativos fundada em 2001, 
+ um modelo tal qual da Apache Software Foundation.
+
+No lançamento incluiu a mudança na especificação para 
+suportar escopo aninhado, assim como outras linguagens 
+com escopo estático. Atualmente funcionalidade estava desativada por padrão, e somente foi requerida na 
+versão 2.2.
+
+Uma grande inovação da versão 2.2 foi a unificação 
+dos tipos Python (escritos em C) e classes (escritas 
+em Python) em somente uma hierarquia. Isto tornou o 
+modelo de objetos do Python consistentemente orientado
+a objeto. Em 01/10/2008 foi lançada a versão 2.6, já 
+visando a transição para a versão 3.0 da linguagem.
+
+Entre outras modificações, foram incluídas bibliotecas 
+para multiprocessamento, JSON e E/S, além de uma nova 
+forma de formatação de cadeias de caracteres.
+
+Python 3
+O Python 3.0 ou Python 3000 foi lançado em dezembro de 
+2008, também chamada Python3. Houve quebra de 
+compatibilidade com a família 2.x para corrigir 
+falhas que foram descobertas neste padrão, e para 
+limpar os excessos das versões anteriores.
+
+A primeira versão alfa foi lançada em 31/08/2007, a 
+segunda em 07/12/2007. Várias mudanças foram feitas, 
+inclindo a alteração da palavra reservada print, que 
+passa a ser uma função, tornando mais fácil a utilização 
+de uma versão alternativa da rotina.
+
+Python influenciou várias linguagens, algumas delas 
+foram Boo e Cobra, que usa a indentação como definição 
+de bloco e Go, que se baseia nos princípios de 
+desenvolvimento rápido de Python. Em 2012, foi criado 
+o Raspberry Pi tendo em mente a linguagem Python.
+
+Atualmente, Python é um dos componentes padrão de 
+vários sistemas operacionais, entre eles estão a 
+maioria das distribuições do Linux, AmigaOS 4, 
+FreeBSD, NetBSD, OpenBSD e OS X. A linguagem se 
+tornou a padrão no curso de ciências da computação 
+do MIT em 2009. Além disso, muitas empresas de grande
+ sucesso utilizam Python em seus projetos. Alguns 
+ exemplos que usam MUITO Python: Google, Youtube, 
+ Nasa, Disney, e no Brasil: Magazine Luiza, Locaweb, 
+ globo.com.
+
+Esse texto foi extraído dos escriitos de Welton Vaz.
+https://www.eusoudev.com.br/python-como-surgiu/
+
---
+++ curso_python#01.py/seja-um-programador.py
@@ -0,0 +1,35 @@
+C.O.De. com - daqui 10 anos teremos 1.4 milão 
+de programadores em falta.
+Tudo é controlado por algoritmos.
+Todas pessoas deveriam aprender a programar, 
+porque ensina a pensar;
+Ensina a ser mais criativo, ter raciocínio, 
+crítica e criar novas coisas;
+Resolver problemas mais rápidos;
+Espírito de comunidade;
+Compartilhar ideias;
+Aprender as coisas de forma mais rápidas;
+Programar é coisa de pessoas com perfis inteligentes.
+
+Obstáculos:
+
+Falta de compromisso (procrastinação);
+Excesso de pressa;
+Poucos exercícios práticos;
+Não valorizar a base;
+Achar que já sabe tudo;
+Programar não é dificil, mas, necessita de compromisso.
+
+Princípios profissionais
+
+Desktop;
+Web;
+Mobile;
+IOT (Internet das Coisas) - Internet of Things;
+Games.
+
+Um novo "idioma"
+
+Python/C++/Ruby/ASP/Java/JS/PHP/Swift
+
+Focaremos na linguagem Python
